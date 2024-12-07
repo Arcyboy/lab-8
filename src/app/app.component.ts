d005be16aedc0c876f1c67da7b3a84fe8e475c0b
Arc Joseph Cruz <arcjosephcruz.neust@gmail.com>
2024-12-07 20:36:45 +0800

    dasdasd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,30 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  Q: number | null = null; 
+  W: number | null = null; 
+  deltaU: number | null = null; 
+  result: string | null = null;
+
+  compute() {
+    if (this.Q !== null && this.W !== null) {
+      
+      this.deltaU = this.Q - this.W;
+      this.result = `ΔU = ${this.deltaU.toFixed(2)} J`;
+    } else if (this.deltaU !== null && this.W !== null) {
+    
+      this.Q = this.deltaU + this.W;
+      this.result = `Q = ${this.Q.toFixed(2)} J`;
+    } else if (this.deltaU !== null && this.Q !== null) {
+      
+      this.W = this.Q - this.deltaU;
+      this.result = `W = ${this.W.toFixed(2)} J`;
+    } else {
+      this.result = 'Please provide at least two values.';
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
-import { AppRoutingModule } from './app-routing.module';
+import { RouteReuseStrategy } from '@angular/router';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    FormsModule,
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
